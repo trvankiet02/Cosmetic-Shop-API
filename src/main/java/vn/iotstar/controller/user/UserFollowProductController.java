5056627a057c564e2ffb5fe6bf5cb7fe2f813206
20110665 <20110665@student.hcmute.edu.vn>
2023-05-09 17:14:58 +0700

    update follow and unfollow

--- src/main/java/vn/iotstar/controller/user/UserFollowProductController.java
+++ src/main/java/vn/iotstar/controller/user/UserFollowProductController.java
@@ -30,40 +30,52 @@ public class UserFollowProductController {
 
 	@Autowired
 	private UserFollowProductRepository userFollowProductRepository;
-	
-	@Autowired 
+
+	@Autowired
 	private UserRepository userRepository;
-	
-	@Autowired 
+
+	@Autowired
 	private ProductRepository productRepository;
-	
-	@PostMapping(path = "/getUserFollowProduct")
-	public ResponseEntity<?> getUserFollowProduct(@Validated @RequestParam("userId") Integer userId){
+
+	@PostMapping(path = "/getFollowProduct")
+	public ResponseEntity<?> getUserFollowProduct(@Validated @RequestParam("userId") Integer userId) {
 		Optional<User> optUser = userRepository.findById(userId);
 		if (optUser.isEmpty()) {
-			return new ResponseEntity<Response>(new Response(false, "Người dùng không tồn tại", null), HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<Response>(new Response(false, "Người dùng không tồn tại", null),
+					HttpStatus.BAD_REQUEST);
 		} else {
 			List<UserFollowProduct> userFollowProductList = userFollowProductRepository.findByUser(optUser.get());
 			return new ResponseEntity<Response>(new Response(true, "Thành công", userFollowProductList), HttpStatus.OK);
 		}
 	}
-	@PostMapping(path = "/addUserFollowProduct")
+
+	@PostMapping(path = "/followOrUnfollow")
 	public ResponseEntity<?> addUserFollowProduct(@Validated @RequestParam("userId") Integer userId,
-			@Validated @RequestParam("productId") Integer productId){
+			@Validated @RequestParam("productId") Integer productId) {
+
 		Timestamp timestamp = new Timestamp(new Date(System.currentTimeMillis()).getTime());
-		
-		UserFollowProduct userFollowProduct = new UserFollowProduct();
-		
+
 		Optional<User> optUser = userRepository.findById(userId);
 		Optional<Product> optProduct = productRepository.findById(productId);
-		
-		if (optUser.isEmpty()) {
-			return new ResponseEntity<Response>(new Response(false, "Người dùng không tồn tại", null), HttpStatus.BAD_REQUEST);
-		} else {
-			userFollowProduct.setCreateAt(timestamp);
-			userFollowProduct.setProduct(optProduct.get());
-			userFollowProduct.setUser(optUser.get());
+
+		Optional<UserFollowProduct> optUserFollowProduct = userFollowProductRepository
+				.findByUserAndProduct(optUser.get(), optProduct.get());
+
+		if (optUserFollowProduct.isPresent()) {
+			userFollowProductRepository.delete(optUserFollowProduct.get());
 			return new ResponseEntity<Response>(new Response(true, "Thành công", null), HttpStatus.OK);
+		} else {
+			UserFollowProduct userFollowProduct = new UserFollowProduct();
+
+			if (optUser.isEmpty()) {
+				return new ResponseEntity<Response>(new Response(false, "Người dùng không tồn tại", null),
+						HttpStatus.BAD_REQUEST);
+			} else {
+				userFollowProduct.setCreateAt(timestamp);
+				userFollowProduct.setProduct(optProduct.get());
+				userFollowProduct.setUser(optUser.get());
+				return new ResponseEntity<Response>(new Response(true, "Thành công", null), HttpStatus.OK);
+			}
 		}
 	}
 }
--- src/main/java/vn/iotstar/repository/UserFollowProductRepository.java
+++ src/main/java/vn/iotstar/repository/UserFollowProductRepository.java
@@ -1,15 +1,18 @@
 package vn.iotstar.repository;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import vn.iotstar.entity.Product;
 import vn.iotstar.entity.User;
 import vn.iotstar.entity.UserFollowProduct;
 
 @Repository
 public interface UserFollowProductRepository extends JpaRepository<UserFollowProduct, Integer>{
 	List<UserFollowProduct> findByUser(User user);
+	
+	Optional<UserFollowProduct> findByUserAndProduct (User user, Product product);
 }
