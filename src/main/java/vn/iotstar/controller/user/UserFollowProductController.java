d210fda5efb98334092ed462a99a97ff49e7dc14
20110665 <20110665@student.hcmute.edu.vn>
2023-05-09 17:09:30 +0700

    user can follow product

--- src/main/java/vn/iotstar/controller/user/UserFollowProductController.java
+++ src/main/java/vn/iotstar/controller/user/UserFollowProductController.java
@@ -1,7 +1,11 @@
 package vn.iotstar.controller.user;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,8 +17,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.iotstar.Response;
+import vn.iotstar.entity.Product;
 import vn.iotstar.entity.User;
 import vn.iotstar.entity.UserFollowProduct;
+import vn.iotstar.repository.ProductRepository;
 import vn.iotstar.repository.UserFollowProductRepository;
 import vn.iotstar.repository.UserRepository;
 
@@ -28,14 +34,36 @@ public class UserFollowProductController {
 	@Autowired 
 	private UserRepository userRepository;
 	
+	@Autowired 
+	private ProductRepository productRepository;
+	
 	@PostMapping(path = "/getUserFollowProduct")
 	public ResponseEntity<?> getUserFollowProduct(@Validated @RequestParam("userId") Integer userId){
 		Optional<User> optUser = userRepository.findById(userId);
 		if (optUser.isEmpty()) {
-			return new ResponseEntity<Response>(new Response(true, "Người dùng không tồn tại", null), HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<Response>(new Response(false, "Người dùng không tồn tại", null), HttpStatus.BAD_REQUEST);
 		} else {
 			List<UserFollowProduct> userFollowProductList = userFollowProductRepository.findByUser(optUser.get());
 			return new ResponseEntity<Response>(new Response(true, "Thành công", userFollowProductList), HttpStatus.OK);
+		}
+	}
+	@PostMapping(path = "/addUserFollowProduct")
+	public ResponseEntity<?> addUserFollowProduct(@Validated @RequestParam("userId") Integer userId,
+			@Validated @RequestParam("productId") Integer productId){
+		Timestamp timestamp = new Timestamp(new Date(System.currentTimeMillis()).getTime());
+		
+		UserFollowProduct userFollowProduct = new UserFollowProduct();
+		
+		Optional<User> optUser = userRepository.findById(userId);
+		Optional<Product> optProduct = productRepository.findById(productId);
+		
+		if (optUser.isEmpty()) {
+			return new ResponseEntity<Response>(new Response(false, "Người dùng không tồn tại", null), HttpStatus.BAD_REQUEST);
+		} else {
+			userFollowProduct.setCreateAt(timestamp);
+			userFollowProduct.setProduct(optProduct.get());
+			userFollowProduct.setUser(optUser.get());
+			return new ResponseEntity<Response>(new Response(true, "Thành công", null), HttpStatus.OK);
 		}
 	}
 }
