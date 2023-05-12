c09f673069092d783a809a0f5674d9e5da15ff54
20110665 <20110665@student.hcmute.edu.vn>
2023-05-12 20:02:37 +0700

    update ordercontroller

--- src/main/java/vn/iotstar/controller/order_delivery/OrderController.java
+++ src/main/java/vn/iotstar/controller/order_delivery/OrderController.java
@@ -64,76 +64,88 @@ public class OrderController {
 
 	@PostMapping(path = "/getOrder")
 	public ResponseEntity<?> getOrder(@Validated @RequestParam("userId") Integer userId,
-			@Validated @RequestParam("status") Integer status) {
+			@RequestParam(value = "status", required = false) Integer status) {
 		Optional<User> optUser = userRepository.findById(userId);
-
-		//return ResponseEntity.ok().body(orderRepository.findByUserAndStatus(optUser.get(), status));
-		return new ResponseEntity<Response>(new Response(true, "Thành công", orderRepository.findByUserAndStatus(optUser.get(), status)), HttpStatus.OK);
+		if (optUser.isPresent()) {
+			if (status == null) {
+				return new ResponseEntity<Response>(
+						new Response(true, "Thành công", orderRepository.findByUser(optUser.get())),
+						HttpStatus.OK);
+			} else {
+				// return
+				// ResponseEntity.ok().body(orderRepository.findByUserAndStatus(optUser.get(),
+				// status));
+				return new ResponseEntity<Response>(
+						new Response(true, "Thành công", orderRepository.findByUserAndStatus(optUser.get(), status)),
+						HttpStatus.OK);
+			}
+		} else {
+			return new ResponseEntity<Response>(new Response(false, "Người dùng không tồn tại", null), HttpStatus.BAD_REQUEST);
+			
+		}
 	}
-	
+
 	@PostMapping(path = "/addOrder")
 	public ResponseEntity<?> addOrder(@Validated @RequestParam("cartItemIdList") List<Integer> cartItemIdList,
-	                                   @Validated @RequestParam("userId") Integer userId,
-	                                   @Validated @RequestParam("name") String name,
-	                                   @Validated @RequestParam("deliveryId") Integer deliveryId,
-	                                   @Validated @RequestParam("address") String address,
-	                                   @Validated @RequestParam("phone") String phone) {
-	    Optional<User> optUser = userRepository.findById(userId);
-	    Optional<Delivery> optDelivery = deliveryRepository.findById(deliveryId);
-	    Timestamp timestamp = new Timestamp(new Date(System.currentTimeMillis()).getTime());
-	    List<CartItem> cartItemList = new ArrayList<>();
-	    List<Order> orderList = new ArrayList<>();
+			@Validated @RequestParam("userId") Integer userId, @Validated @RequestParam("name") String name,
+			@Validated @RequestParam("deliveryId") Integer deliveryId,
+			@Validated @RequestParam("address") String address, @Validated @RequestParam("phone") String phone) {
+		Optional<User> optUser = userRepository.findById(userId);
+		Optional<Delivery> optDelivery = deliveryRepository.findById(deliveryId);
+		Timestamp timestamp = new Timestamp(new Date(System.currentTimeMillis()).getTime());
+		List<CartItem> cartItemList = new ArrayList<>();
+		List<Order> orderList = new ArrayList<>();
 
-	    // Lấy danh sách các CartItem từ danh sách ID được cung cấp
-	    for (Integer cartItemId : cartItemIdList) {
-	        Optional<CartItem> optCartItem = cartItemRepository.findById(cartItemId);
-	        cartItemList.add(optCartItem.get());
-	    }
+		// Lấy danh sách các CartItem từ danh sách ID được cung cấp
+		for (Integer cartItemId : cartItemIdList) {
+			Optional<CartItem> optCartItem = cartItemRepository.findById(cartItemId);
+			cartItemList.add(optCartItem.get());
+		}
 
-	    // Tạo danh sách các đơn hàng dựa trên Store và lưu vào cơ sở dữ liệu
-	    Map<Integer, Order> orderMap = new HashMap<>();
-	    for (CartItem cartItem : cartItemList) {
-	        Integer storeId = cartItem.getProduct().getStore().getId();
-	        if (!orderMap.containsKey(storeId)) {
-	            Order order = new Order();
-	            order.setUser(optUser.get());
-	            order.setDelivery(optDelivery.get());
-	            order.setStore(storeRepository.findById(storeId).get());
-	            order.setAddress(address.trim());
-	            order.setName(name.trim());
-	            order.setPhone(phone.trim());
-	            order.setPrice(priceEstimate(cartItemList, storeId));
-	            order.setStatus(1);
-	            order.setCreateAt(timestamp);
-	            orderMap.put(storeId, order);
-	        }
-	        
-	    }
-	    for (Order order : orderMap.values()) {
-	        orderList.add(orderRepository.save(order));
-	    }
+		// Tạo danh sách các đơn hàng dựa trên Store và lưu vào cơ sở dữ liệu
+		Map<Integer, Order> orderMap = new HashMap<>();
+		for (CartItem cartItem : cartItemList) {
+			Integer storeId = cartItem.getProduct().getStore().getId();
+			if (!orderMap.containsKey(storeId)) {
+				Order order = new Order();
+				order.setUser(optUser.get());
+				order.setDelivery(optDelivery.get());
+				order.setStore(storeRepository.findById(storeId).get());
+				order.setAddress(address.trim());
+				order.setName(name.trim());
+				order.setPhone(phone.trim());
+				order.setPrice(priceEstimate(cartItemList, storeId));
+				order.setStatus(1);
+				order.setCreateAt(timestamp);
+				orderMap.put(storeId, order);
+			}
 
-	    // Tạo danh sách các OrderItem và lưu vào cơ sở dữ liệu
-	    for (CartItem cartItem : cartItemList) {
-	        OrderItem orderItem = new OrderItem();
-	        orderItem.setProduct(cartItem.getProduct());
-	        orderItem.setSize(cartItem.getSize());
-	        orderItem.setQuantity(cartItem.getQuantity());
-	        orderItem.setCreateAt(timestamp);
-	        orderItem.setUnitPrice(cartItem.getQuantity() * cartItem.getProduct().getPromotionalPrice());
-	        Order order = orderMap.get(cartItem.getProduct().getStore().getId());
-	        orderItem.setOrder(order);
-	        orderItemRepository.save(orderItem);
-	    }
+		}
+		for (Order order : orderMap.values()) {
+			orderList.add(orderRepository.save(order));
+		}
 
-	    // Cập nhật trạng thái của các CartItem
-	    for (CartItem cartItem : cartItemList) {
-	        cartItemRepository.delete(cartItem);
-	    }
-	    //return ResponseEntity.ok().body(orderRepository.findByCreateAt(timestamp));
-	    return new ResponseEntity<Response>(new Response(true, "Thành công", orderRepository.findByCreateAt(timestamp)), HttpStatus.OK);
+		// Tạo danh sách các OrderItem và lưu vào cơ sở dữ liệu
+		for (CartItem cartItem : cartItemList) {
+			OrderItem orderItem = new OrderItem();
+			orderItem.setProduct(cartItem.getProduct());
+			orderItem.setSize(cartItem.getSize());
+			orderItem.setQuantity(cartItem.getQuantity());
+			orderItem.setCreateAt(timestamp);
+			orderItem.setUnitPrice(cartItem.getQuantity() * cartItem.getProduct().getPromotionalPrice());
+			Order order = orderMap.get(cartItem.getProduct().getStore().getId());
+			orderItem.setOrder(order);
+			orderItemRepository.save(orderItem);
+		}
+
+		// Cập nhật trạng thái của các CartItem
+		for (CartItem cartItem : cartItemList) {
+			cartItemRepository.delete(cartItem);
+		}
+		// return ResponseEntity.ok().body(orderRepository.findByCreateAt(timestamp));
+		return new ResponseEntity<Response>(new Response(true, "Thành công", orderRepository.findByCreateAt(timestamp)),
+				HttpStatus.OK);
 	}
-
 
 	public Integer priceEstimate(List<CartItem> cartItemList, Integer storeId) {
 		Integer sum = 0;
